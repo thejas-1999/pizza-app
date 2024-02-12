cdea8acb6ecd070068bb098f7ec04c29067b9c31
thejas-1999 <thejask074@gmail.com>
2024-02-12 16:29:55 +0530

    install react router dom and add styles to nav bar

--- pizza-app/src/App.jsx
+++ pizza-app/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- pizza-app/src/Components/NavBar.jsx
+++ pizza-app/src/Components/NavBar.jsx
@@ -1,11 +1,19 @@
-import Logo from "../Asssets/pizzaLogo.png";
+import "../Styles/NavBar.css";
+import Logo from "../Asssets/gtec.png";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="navbar">
       <div className="leftSide">
         <img src={Logo} alt="logo" />
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   );
 };
