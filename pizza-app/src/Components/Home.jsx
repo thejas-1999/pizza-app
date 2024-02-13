26bf38b91ecf56e06a6f247d95293afaa202aa33
thejas-1999 <thejask074@gmail.com>
2024-02-13 09:56:01 +0530

    Add the home component and styles it

--- pizza-app/src/Components/Home.jsx
+++ pizza-app/src/Components/Home.jsx
@@ -1,7 +1,19 @@
+import "../Styles/Home.css";
+import { Link } from "react-router-dom";
+import BannerImage from "../Asssets/pizza.jpeg";
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div
+        className="headerContainer"
+        style={{ background: `url(${BannerImage})` }}
+      >
+        <h1>TK </h1>
+        <h2>lets go</h2>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
     </div>
   );
 };
